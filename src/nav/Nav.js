f02350dd43b3923f0395280b85127e9df97cbde6
Dzmitry <maddal.privod@gmail.com>
2022-03-23 16:29:27 +0300

    Create scrolling

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -3,15 +3,33 @@ import s from './Nav.module.css';
 
 export const Nav = () => {
 
+    // const pages = [
+    //     {title: 'Home', x:0, y:0},
+    //     {title: 'About', x:0, y: document.getElementById('asfa')},
+    //     {title: 'Skills', x:0, y: 697},
+    //     {title: 'Projects', x:0, y: 1343},
+    //     {title: 'Contact', x:0, y: 1845},
+    //     {title: 'GitHub', x:0, y: 0}
+    // ]
+    const pages = [
+        {id: 'main',title: 'Home'},
+        {id: 'profile',title: 'About'},
+        {id: 'skills',title: 'Skills'},
+        {id: 'projects',title: 'Projects'},
+        {id: 'contacts',title: 'Contact'},
+        {id: 'main',title: 'GitHub'}
+    ]
+    const onClickHandler = (id) => document.
+    getElementById(id).
+    scrollIntoView({block: "start",behavior: 'smooth'})
+
     return (
         <div className={s.nav}>
-
-            <a href={'#main'}>Home</a>
-            <a href={'#profile'}>About</a>
-            <a href={'#skills'}>Skills</a>
-            <a href={'#projects'}>Projects</a>
-            <a href={'#contacts'}>Contact</a>
-            <a href=''>GitHub</a>
+            {pages.map( p => <div
+                title={'text'}
+                className={s.div}
+                onClick={()=>onClickHandler(p.id)}
+            >{p.title}</div>)}
         </div>
     )
 }
