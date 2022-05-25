ebfd51dc26dfd4439696437403f3743dc35a5e2a
Dzmitry <maddal.privod@gmail.com>
2022-05-25 20:59:49 +0300

    Added logos for skills

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,18 +1,28 @@
 import s from './Skills.module.css';
 import {Skill} from "./skill/Skill";
 import {v1} from "uuid";
+import html from '../assets/images/html5_logo.png'
+import css from '../assets/images/css3_logo.png'
+import js from '../assets/images/JS_logo.png'
+import ts from '../assets/images/TS_logo.jpg'
+import react from '../assets/images/react_logo.png'
+import redux from '../assets/images/redux_logo.png'
+import git from '../assets/images/git_logo.png'
+import sass from '../assets/images/sass_logo.png'
+import postman from '../assets/images/postman_logo.png'
 
 export const Skills = () => {
 
     const skills = [
-        {id:v1(),title:'HTML',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'CSS',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'JavaScript',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'TypeScript',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'React',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'Redux',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'Git',img:'https://ksim.ru/img/empty-square.png'},
-        {id:v1(),title:'SASS',img:'https://ksim.ru/img/empty-square.png'},
+        {id:v1(),title:'HTML',img:html},
+        {id:v1(),title:'CSS',img:css},
+        {id:v1(),title:'JavaScript',img:js},
+        {id:v1(),title:'TypeScript',img:ts},
+        {id:v1(),title:'React',img:react},
+        {id:v1(),title:'Redux',img:redux},
+        {id:v1(),title:'Git',img:git},
+        {id:v1(),title:'SASS',img:sass},
+        {id:v1(),title:'Postman',img:postman},
     ]
 
     return (
