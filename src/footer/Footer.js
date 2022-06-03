27d7831d406d1889f147a1c9fd4533de7ff5373b
Dzmitry <maddal.privod@gmail.com>
2022-06-03 12:35:27 +0300

    Ref

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -7,9 +7,9 @@ import telegram from '../assets/images/logo_telegram.png'
 export const Footer = () => {
 
     const footerContainer = [
-        {name:'linkedIn', logo:linkedIn, link:'https://www.linkedin.com/in/dzmitry-yakubyshyn-432267235/'},
-        {name:'gitHub', logo:git, link:'https://github.com/Dallmad'},
-        {name:'telegram', logo:telegram, link:'https://github.com/Dallmad'},
+        {name:'linkedIn', logo: linkedIn, link:'https://www.linkedin.com/in/dzmitry-yakubyshyn-432267235/'},
+        {name:'gitHub', logo: git, link:'https://github.com/Dallmad'},
+        {name:'telegram', logo: telegram, link:'https://github.com/Dallmad'},
     ]
 
     const logoHandler = (link) => {
