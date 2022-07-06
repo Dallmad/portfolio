d3d183cf60edeb6eac86236294e60eb8c968f353
Dzmitry <maddal.privod@gmail.com>
2022-07-06 23:04:02 +0300

    Rework contact form

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,5 +2,4 @@ import axios from 'axios';
 
 export const instance = axios.create({
     baseURL: 'https://smtp-server-dallmad.herokuapp.com/',
-    withCredentials: true
 })
--- src/contacts/data/Data/Data.js
+++ src/contacts/data/Data/Data.js
@@ -12,53 +12,15 @@ export const Data = () => {
         email: '',
         message: '',
     })
-    const [validation, setValidation] = useState({
-        name: '',
-        email: '',
-        message: '',
-    })
+    const [feedback, setFeedback] = useState('')
     const [disable, setDisable] = useState(false)
+    const [error, setError] = useState(false)
 
     const handleChange = (e) => {
         const {name, value} = e.target
         setInputValue({...inputValues, [name]: value})
     }
 
-    const checkValidation = () => {
-        let errors = validation
-
-        //name validation
-        const nameCond = /^([A-Z]{2,40})$/i
-        if (!inputValues.name.trim()) {
-            errors.name = 'You need to enter your name'
-        } else if (!nameCond.test(inputValues.name)) {
-            errors.name = 'Name must contain from 2 to 40 Latin letters'
-        } else {
-            errors.name = ''
-        }
-
-        //email validation
-        const emailCond = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/i
-        if (!inputValues.email.trim()) {
-            errors.email = 'Email is required'
-        } else if (!inputValues.email.match(emailCond)) {
-            errors.email = 'Please enter a valid email address'
-        } else {
-            errors.email = ''
-        }
-
-        //message validation
-        if (!inputValues.message.trim()) {
-            errors.message = 'Message is required'
-        } else if (inputValues.message.length < 5) {
-            errors.message = 'Message must be longer than 5 characters'
-        } else if (inputValues.message.length > 300) {
-            errors.message = 'Message must be shorter than 300 characters'
-        } else {
-            errors.message = ''
-        }
-        setValidation(errors)
-    }
     const newMessage = {
         id: v1(),
         name: '',
@@ -66,68 +28,77 @@ export const Data = () => {
         message: '',
     }
 
-    useEffect(() => {
-        checkValidation()
-        if (validation.message
-            || validation.email
-            || validation.name) {
-            setDisable(true)
-        } else setDisable(false)
-        //setInputValue(newMessage)
-    }, [inputValues.message, inputValues.email, inputValues.name,
-        validation.message, validation.email, validation.name
-    ])
-     const sendMessageTC = async (data) => {
+    const sendMessage = async (data) => {
+        setDisable(true)
         try {
             await api.sendMessage(data)
-        } catch (error) {
-            if (error instanceof Error) {
-                console.log(error.name + ': ' + error.message);
+            setError(false)
+            setDisable(false)
+            setFeedback('Your message has been sent ')
+            setInputValue(newMessage)
+        } catch (err) {
+            if (err instanceof Error) {
+                setDisable(false)
+                setError(true)
+                setFeedback(`${err.message}`)
             }
         }
     }
-        const handleSubmit = (e) => {
-            sendMessageTC(inputValues)
-            e.preventDefault()
-        }
-        return (
-            <form
-                className={s.data}
-                id={'formMessage'}
-                onSubmit={handleSubmit}
-            >
-                <div className={s.inputBlock}>
-                    <input
-                        type='text'
-                        placeholder='Your Name'
-                        className={s.input}
-                        name={'name'}
-                        onChange={(e) => handleChange(e)}
-                    />
-                    {validation.name && <p>{validation.name}</p>}
-                    <input
-                        type='email'
-                        placeholder='Your Email'
-                        className={s.input}
-                        name={'email'}
-                        onChange={(e) => handleChange(e)}
-                    />
-                    {validation.email && <p>{validation.email}</p>}
-                </div>
-                <textarea
-                    placeholder='Your Message'
-                    className={s.text}
-                    name={'message'}
-                    onChange={(e) => handleChange(e)}
-                />
-                {validation.message && <p>{validation.message}</p>}
-                <button
-                    type='submit'
-                    className={s.button}
-                    disabled={disable}
-                >
-                    Send Message
-                </button>
-            </form>
-        )
+
+    const handleSubmit = (e) => {
+        sendMessage(inputValues)
+        e.preventDefault()
     }
+
+    useEffect(() => {
+        if (feedback) {
+            const timer = setTimeout(() => {
+                setFeedback('')
+                setError(false)
+            }, 3000)
+            return () => clearTimeout(timer)
+        }
+    }, [feedback])
+
+    return (
+        <form
+            className={s.data}
+            id={'formMessage'}
+            onSubmit={handleSubmit}
+        >
+            <div className={s.inputBlock}>
+                <input
+                    type='text'
+                    placeholder='Your Name'
+                    className={s.input}
+                    name={'name'}
+                    onChange={(e) => handleChange(e)}
+                    value={inputValues.name}
+                />
+                <input
+                    type='email'
+                    placeholder='Your email'
+                    className={s.input}
+                    name={'email'}
+                    onChange={(e) => handleChange(e)}
+                    value={inputValues.contacts}
+                />
+            </div>
+            <textarea
+                placeholder='Your Message'
+                className={s.text}
+                name={'message'}
+                onChange={(e) => handleChange(e)}
+                value={inputValues.message}
+            />
+            <button
+                type='submit'
+                className={s.button}
+                disabled={disable}
+            >
+                Send Message
+            </button>
+            {feedback && <div className={error ? s.feedback_error : s.feedback}>{feedback}</div>}
+        </form>
+    )
+}
